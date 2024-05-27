45b5cc55798e2ac32c1cec68af6a2dc8159e722b
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-05-28 01:23:51 +0200

    refactor: removed last function

--- films.py
+++ films.py
@@ -37,34 +37,4 @@ def get_sorties_de_la_semaine(url: str) -> Dict[str, str]:
 
 
 def get_critiques(url: str) -> pd.DataFrame:
-    soup = fetch_and_parse(url)
-    name_critic = soup.find_all(
-        "a", {"class": "sc-e6f263fc-0 sc-b7da4c5c-2 GItpw gvGKDY"}
-    )
-    likes_critic = soup.find_all(
-        "p", {"class": "sc-e6f263fc-0 sc-8d1083e0-1 GItpw XTOao"}
-    )
-    comments_critic = soup.find_all(
-        "p", {"class": "sc-e6f263fc-0 sc-3fcb6f61-1 GItpw djcKen"}
-    )
-    content_critic = soup.find_all(
-        "div", {"class": "sc-8251ce8c-3 gbAIun sc-4db36029-3 iepouY"}
-    )
-    rating_critic = soup.find_all(
-        "div", {"class": "sc-8251ce8c-3 gbAIun sc-4db36029-3 iepouY"}
-    )
-    photos_critic = soup.find_all("a", {"class": "sc-1a97d7be-3 fZBrFn"})
-    date_critic = soup.find_all(
-        "p", {"class": "sc-e6f263fc-0 sc-3b5d9ff1-0 GItpw dOiIbg"}
-    )
-    data = {
-        "Dates": [date.text for date in date_critic],
-        "Name": [name.text for name in name_critic],
-        "Likes": [like.text for like in likes_critic],
-        "Comments": [comment.text for comment in comments_critic],
-        "Content": [content.text for content in content_critic],
-        "Rating": [rating.text for rating in rating_critic],
-        "Photos": [photo.text for photo in photos_critic],
-    }
-    df = pd.DataFrame(data)
-    return df
+    pass
